dbcd5d4a9adbb7836420578e58ad06d8873cf8ed
Rocio <rociogonzalezbusai@gmail.com>
2023-03-24 00:08:40 -0300

    creando mi primer repositorio

---
+++ script.js
@@ -0,0 +1,30 @@
+//Bucles for
+
+/* for(let i = 1; i <= 300; i = i + 1){
+    console.log("la variable i vale: " + i)
+    console.log("hola mundo")
+}
+*/
+
+//ARRAYS
+
+//Mostrame por consola a cada alumno
+/*
+const listaDeAlumnos = ["Paula","Maria","pepe","Facundo"]
+
+for(let i = 0; i < listaDeAlumnos.length; i = i + 1){
+    console.log(listaDeAlumnos[i])
+}*/
+
+let nombre = "Pepe"
+if(true){
+    let nombre ="Lucas"
+    if(true){
+        console.log(nombre)
+    }
+    console.log(nombre)
+    }
+
+console.log(nombre)
+
+/* Lucas Lucas Pepe */
